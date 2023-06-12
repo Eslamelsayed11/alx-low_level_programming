92d075119cd18bae9f96a0eef56964764a5ea53e
Eslamelsayed11 <eslamelsayedfawzy0123@gmail.com>
2023-06-12 13:25:52 +0300

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ee = malloc(sizeof (int*) * height);
+	ee = malloc(sizeof(int *) * height);
 
 	if (ee == NULL)
 		return (NULL);
